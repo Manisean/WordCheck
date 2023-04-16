0e71fa38408c64e3e1da993599426a5b2c42af5d
Andrew-Vinci <andrew.vinci@mymail.champlain.edu>
2023-04-16 01:40:08 -0400

    still working on tests

--- Test/test.cpp
+++ Test/test.cpp
@@ -1,81 +1,38 @@
 #include "pch.h"
 //replace with path to Header.h
-#include "C:\Users\markh\source\repos\WordCheck\WordCheck\Header.h"
+
+#include "C:\Users\andre\OneDrive\Desktop\DevM\WordCheck\Header.h"
 
 // Test to make sure everything is working correctly
 
 // Test to check for all upper case words in text file
 TEST(WordCheckTest, UpperTest) {
+	string filename = "checkUpper3.txt";
 
-	string line, word;
-	ifstream file("inputFile.txt");
+	EXPECT_TRUE(checkUpper(filename));
 
-	if (!file.is_open()) {
-		cout << "Error Opening File,\n";
-		FAIL();
-	}
-
-	while (getline(file, line)) {
-		istringstream ss(line);
-		while (ss >> word) {
-			for (int i = 0; i < word.length(); i++) {
-				EXPECT_TRUE(isupper(word[i]));
-			}
-		}
-	}
-
-	file.close();
-
+	remove(filename.c_str());
 }
+
+
 
 // Test for a provided word to be replaced with a random word from a separate text file
 TEST(WordCheckTest, RandomTest) {
+	
+	string filename = "fox.txt";
+	replaceWithRandom(filename, "fox");
 
-	string word = "WORD";
-	string line, line2, word2;
-	bool check = false;
-	int wordCount = 0;
-	ifstream file("inputFile.txt");
-	ifstream file2("words.txt");
+	// Read the file and make sure "fox" has been replaced
+	ifstream infile(filename);
+	string line;
+	getline(infile, line);
+	infile.close();
 
-	while (file2 >> word) {
-		wordCount++;
-	}
-
-	srand(time(NULL));
-	int randomNumber = rand() % wordCount + 1;
-
-	if (!file.is_open()) {
-		cout << "Error Opening File\n";
-		FAIL();
-	}
-
-	if (!file2.is_open()) {
-		cout << "Error Opening File2\n";
-		FAIL();
-	}
-
-	while (getline(file, line)) {
-		istringstream ss(line);
-		while (ss >> word) {
-			if (word == "WORD") {
-				while (getline(file2, line2)) {
-					istringstream ss2(line2);
-					while (ss2 >> word2) {
-						if (wordCount == randomNumber) {
-							word = word2;
-							SUCCEED();
-						}
-					}
-				}
-			}
-		}
-	}
-
-	FAIL();
-
-	file.close();
+	EXPECT_NE(line.find("fox"), std::string::npos) << "Original word not found in file.";
+	EXPECT_EQ(line.find("fox"), std::string::npos) << "Replaced word found in file.";
 }
+
+
 
 // check if words in given file have an extra letter
 TEST(WordCheckTest, ExtraCharTrue) {
@@ -84,7 +41,7 @@ TEST(WordCheckTest, ExtraCharTrue) {
 
 	getline(inputFile, inputLine);
 
-	if (extraChar(inputLine) == true) {
+	if (extraChar(inputLine)) {
 		SUCCEED();
 	}
 	else {
@@ -118,7 +75,7 @@ TEST(WordCheckTest, SwearPresent) {
 
 	getline(inputFile, inputLine);
 
-	if (swears(inputLine) == true) {
+	if (swears(inputLine)) {
 		SUCCEED();
 	}
 	else {
@@ -127,6 +84,8 @@ TEST(WordCheckTest, SwearPresent) {
 
 	inputFile.close();
 }
+
+
 
 // check if file is free of swear words
 TEST(WordCheckTest, NoSwearPresent) {
--- WordCheck/Header.h
+++ WordCheck/Header.h
@@ -3,17 +3,45 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 using namespace std;
 
 
 // swear filter func for test
 bool swears(string inputLine) {
 
+    vector<string> swears;
+    string swear;
+    ifstream swearsFile("swears.txt");
+    while (swearsFile >> swear) {
+        swears.push_back(swear);
+    }
+    swearsFile.close();
+
+    for (int i = 0; i < swears.size(); i++) {
+        string w = swears[i];
+        if (inputLine.find(w) != string::npos) {
+            return true;
+        }
+    }
+   
     return false;
 }
 
 // extra char func for test
 bool extraChar(string inputLine) {
+
+    return false;
+}
+
+bool checkUpper(string file) {
+
+
+    return false;
+}
+
+
+bool replaceWithRandom(string fileName, string word) {
 
     return false;
 }
