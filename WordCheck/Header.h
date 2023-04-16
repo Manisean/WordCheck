342021fe17713d97bd20bfa04c2665b81f7f5529
Andrew-Vinci <andrew.vinci@mymail.champlain.edu>
2023-04-16 01:54:22 -0400

    working on functions

--- WordCheck/Header.h
+++ WordCheck/Header.h
@@ -4,24 +4,34 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <sstream>
 using namespace std;
 
 
 // swear filter func for test
 bool swears(string inputLine) {
-
     vector<string> swears;
     string swear;
     ifstream swearsFile("swears.txt");
+
     while (swearsFile >> swear) {
         swears.push_back(swear);
     }
     swearsFile.close();
 
-    for (int i = 0; i < swears.size(); i++) {
-        string w = swears[i];
-        if (inputLine.find(w) != string::npos) {
-            return true;
+    vector<string> words;
+    stringstream ss(inputLine);
+    string word;
+
+    while (ss >> word) {
+        words.push_back(word);
+    }
+
+    for (int i = 0; i < words.size(); i++) {
+        for (int j = 0; j < swears.size(); j++) {
+            if (words[i] == swears[j]) {
+                return true;
+            }
         }
     }
    
